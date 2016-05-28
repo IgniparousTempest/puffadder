caef0c0c9ba79678feebdf11ada2edc450a63f87
Pitcher <pitchec@amazon.com>
2016-05-28 10:24:44 +0200

    Added pip files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='puffadder',
+    packages=['puffadder'],
+    version='0.1',
+    description="A set of boilerplate reducing decorators, based off of Java's Lombok package",
+    author='Courtney Pitcher',
+    author_email='cr.pitcher@gmail.com',
+    url='https://github.com/IgniparousTempest/puffadder',
+    download_url='https://github.com/IgniparousTempest/puffadder/tarball/0.1',
+    keywords=['lombok', 'decorator', 'boiler plate'],
+    classifiers=[],
+)
